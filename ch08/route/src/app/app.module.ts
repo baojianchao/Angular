016b6a680a92f0c4d97568121b301f53d500b160
baojianchao <535141237@qq.com>
2020-05-02 16:37:19 +0800

    2020-05-02

--- ch08/route/src/app/app.module.ts
+++ ch08/route/src/app/app.module.ts
@@ -1,16 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { ManagementComponentComponent } from './management-component/management-component.component';
+import { UserManagementComponentComponent } from './user-management-component/user-management-component.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { ProductComponentComponent } from './product-component/product-component.component';
+import { ExitComponentComponent } from './exit-component/exit-component.component';
+import { LoginGuard } from './login.guard';
+import { AuthService } from './auth.service';
+
+
+const mgtChildrenRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'user', component: UserManagementComponentComponent },
+  { path: 'product', component: ProductComponentComponent },
+  { path: 'exit', component: ExitComponentComponent }
+];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponentComponent },
+  { path: 'login', component: LoginComponentComponent },
+  {
+    path: 'management',
+    component: ManagementComponentComponent,
+    children: mgtChildrenRoutes,
+    canActivate: [LoginGuard]
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent,
+    ManagementComponentComponent,
+    UserManagementComponentComponent,
+    LoginComponentComponent,
+    ProductComponentComponent,
+    ExitComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [LoginGuard, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
